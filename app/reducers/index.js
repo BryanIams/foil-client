4f13aa4e304437144a755948bb1adeba066e6231
Bryan Iams <BryanIams@outlook.com>
2017-10-11 20:09:28 -0700

    Removing some unnecessary files

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
-import counter from './counter';
 import decks from './decks';
 
 const rootReducer = combineReducers({
-  counter,
   router,
   decks
 });
